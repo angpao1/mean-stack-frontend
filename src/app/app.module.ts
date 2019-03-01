776dcf3e2bb612a1d947a0e99e954e4ec6dacc4b
angpao <angpao.kittimasak@gmail.com>
2019-03-01 15:43:19 +0700

    add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AddProductComponent } from './add-product/add-product.component';
 import { EditProductComponent } from './edit-product/edit-product.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'add', component: AddProductComponent },
+]
 
 @NgModule({
   declarations: [
@@ -16,7 +22,8 @@ import { EditProductComponent } from './edit-product/edit-product.component';
     EditProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
