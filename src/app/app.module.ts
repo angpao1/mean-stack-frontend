0d93dcd5048dd242f528292b3dc1865b145ea70c
angpao <angpao.kittimasak@gmail.com>
2019-03-01 15:16:17 +0700

    genarate home component genarate navbarcomponent genarate edit component genarate add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { EditProductComponent } from './edit-product/edit-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AddProductComponent,
+    EditProductComponent
   ],
   imports: [
     BrowserModule
